7b90b775ae690153c6d950f15ae72e692b44d1bd
k-wakisaka <k.wakisaka@vaikobo.com>
2023-12-03 00:01:26 +0900

    first commit

--- vaik_count_pb_inference/pb_model.py
+++ vaik_count_pb_inference/pb_model.py
@@ -5,21 +5,22 @@ import numpy as np
 
 
 class PbModel:
-    def __init__(self, input_saved_model_dir_path: str = None, classes: Tuple = None):
-        self.model = tf.saved_model.load(input_saved_model_dir_path)
-        self.model_input_shape = self.model.signatures["serving_default"].inputs[0].shape
-        self.model_input_dtype = self.model.signatures["serving_default"].inputs[0].dtype
-        self.model_output_count_shape = self.model.signatures["serving_default"].outputs[0].shape
-        self.model_output_count_dtype = self.model.signatures["serving_default"].outputs[0].dtype
-        self.model_output_cam_shape = self.model.signatures["serving_default"].outputs[1].shape
-        self.model_output_cam_dtype = self.model.signatures["serving_default"].outputs[1].dtype
+    def __init__(self, input_saved_model_file_path: str = None, classes: Tuple = None):
+        self.model = tf.keras.models.load_model(input_saved_model_file_path)
+        self.grad_model = self.__prepare_grad_model()
+        self.model_input_shape = self.model.inputs[0].shape
+        self.model_input_dtype = self.model.inputs[0].dtype
+        self.model_output_count_shape = self.model.outputs[0].shape
+        self.model_output_count_dtype = self.model.outputs[0].dtype
+        self.model_output_cam_shape = self.model.outputs[1].shape
+        self.model_output_cam_dtype = self.model.outputs[1].dtype
         self.classes = classes
 
     def inference(self, input_image_list: List[np.ndarray], batch_size: int = 8) -> Tuple[List[Dict], Tuple]:
         resized_image_array, resize_image_shape_list, input_image_shape_list = self.__preprocess_image_list(input_image_list, self.model_input_shape[1:3])
-        output_count_tensor, output_cam_tensor = self.__inference(resized_image_array, batch_size)
-        output = self.__output_parse(output_count_tensor, output_cam_tensor, resize_image_shape_list, input_image_shape_list)
-        return output, (output_count_tensor, output_cam_tensor)
+        output_count_tensor, output_cam_tensor, output_grad_cam_tensor_list = self.__inference(resized_image_array, batch_size)
+        output = self.__output_parse(output_count_tensor, output_cam_tensor, output_grad_cam_tensor_list, resize_image_shape_list, input_image_shape_list)
+        return output, (output_count_tensor, output_cam_tensor, output_grad_cam_tensor_list)
 
     def __inference(self, resize_input_tensor: np.ndarray, batch_size: int) -> np.ndarray:
         if len(resize_input_tensor.shape) != 4:
@@ -28,9 +29,12 @@ class PbModel:
             raise ValueError(f'dtype mismatch expected: {np.uint8}, actual: {resize_input_tensor.dtype}')
 
         output_count_tensor = tf.zeros((resize_input_tensor.shape[0], self.model_output_count_shape[-1]),
-                                 self.model_output_count_dtype).numpy()
-        output_cam_tensor = tf.zeros((resize_input_tensor.shape[0], self.model_output_count_shape[1], self.model_output_count_shape[2], self.model_output_count_shape[-1]),
-                                    self.model_output_count_dtype).numpy()
+                                       self.model_output_count_dtype).numpy()
+        output_cam_tensor = tf.zeros((resize_input_tensor.shape[0], self.model_output_cam_shape[1], self.model_output_cam_shape[2], self.model_output_cam_shape[-1]),
+                                     self.model_output_count_dtype).numpy()
+        output_grad_cam_tensor_list = [tf.zeros((resize_input_tensor.shape[0], output.shape[1], output.shape[2], self.model_output_count_shape[-1]),
+                                                self.model_output_count_dtype).numpy() for output in self.grad_model.outputs[:-2]]
+
         for index in range(0, resize_input_tensor.shape[0], batch_size):
             batch = resize_input_tensor[index:index + batch_size, :, :, :]
             batch_pad = tf.zeros(((batch_size, ) + self.model_input_shape[1:]), self.model_input_dtype).numpy()
@@ -38,7 +42,12 @@ class PbModel:
             raw_count_pred, raw_cam_pred = self.model(batch_pad)
             output_count_tensor[index:index + batch.shape[0], :] = raw_count_pred[:batch.shape[0]]
             output_cam_tensor[index: index + batch.shape[0], :, :, :] = raw_cam_pred[:batch.shape[0]]
-        return output_count_tensor, output_cam_tensor
+        for image_index, resize_input_image in enumerate(resize_input_tensor):
+            for pred_index in range(self.model_output_count_shape[-1]):
+                raw_grad_list = self.__make_gradcam_heatmap(resize_input_image, pred_index)
+                for grad_index, raw_grad in enumerate(raw_grad_list):
+                    output_grad_cam_tensor_list[grad_index][image_index, :, :, pred_index] = raw_grad
+        return output_count_tensor, output_cam_tensor, output_grad_cam_tensor_list
 
     def __preprocess_image_list(self, input_image_list: List[np.ndarray],
                                 resize_input_shape: Tuple[int, int]) -> np.ndarray:
@@ -70,20 +79,72 @@ class PbModel:
         output_image[:resize_image.shape[0], :resize_image.shape[1], :] = resize_image
         return output_image, resize_image.shape, input_image.shape
 
-    def __output_parse(self, output_count_tensor: np.ndarray, output_cam_tensor: np.ndarray, resize_image_shape_list, input_image_shape_list) -> List[Dict]:
+    def __output_parse(self, output_count_tensor: np.ndarray, output_cam_tensor: np.ndarray, output_grad_cam_tensor_list: np.ndarray, resize_image_shape_list, input_image_shape_list) -> List[Dict]:
         output_dict_list = []
-        for index in range(output_count_tensor.shape[0]):
-            resize_cam_canvas = np.zeros(input_image_shape_list[index], dtype=np.float32)
-            for cam_index in range(output_cam_tensor.shape[-1]):
-                org_cam_sum = np.sum(output_cam_tensor[index, :, :, cam_index])
-                cam_input_image = Image.fromarray(output_cam_tensor[index, :, :, cam_index]).resize((self.model_input_shape[2], self.model_input_shape[1]), resample=Image.BICUBIC)
-                crop_cam_image = cam_input_image.crop((0, 0, resize_image_shape_list[index][1], resize_image_shape_list[index][0]))
-                resize_crop_cam_image = np.asarray(crop_cam_image.resize((input_image_shape_list[index][1], input_image_shape_list[index][0])))
-                resize_crop_cam_image = np.clip(resize_crop_cam_image, 0, np.max(resize_crop_cam_image))
-                if org_cam_sum != 0:
-                    resize_crop_cam_image *= org_cam_sum / np.sum(resize_crop_cam_image)
-                resize_cam_canvas[:, :, cam_index] = resize_crop_cam_image
-            output_dict = {'count': [count for count in output_count_tensor[index].tolist()],
-                           'cam': resize_cam_canvas}
+        for image_index in range(output_count_tensor.shape[0]):
+            resize_cam_canvas = np.zeros((input_image_shape_list[image_index][0], input_image_shape_list[image_index][1], output_count_tensor.shape[-1]), dtype=np.float32)
+            for pred_index in range(output_count_tensor.shape[-1]):
+                resize_cam_canvas[:, :, pred_index] = self.__make_count_heatmap(output_cam_tensor[image_index, :, :, pred_index],
+                                                                                resize_image_shape_list[image_index][0], resize_image_shape_list[image_index][1],
+                                                                                input_image_shape_list[image_index][0],
+                                                                                input_image_shape_list[image_index][1], output_count_tensor[image_index, pred_index])
+            resize_grad_cam_canvas_list = []
+            for grad_index in range(len(output_grad_cam_tensor_list)):
+                resize_grad_cam_canvas =   np.zeros((input_image_shape_list[image_index][0], input_image_shape_list[image_index][1], output_count_tensor.shape[-1]), dtype=np.float32)
+                for pred_index in range(output_cam_tensor.shape[-1]):
+                    resize_grad_cam_canvas[:, :, pred_index] = self.__make_count_heatmap(output_grad_cam_tensor_list[grad_index][image_index, :, :, pred_index],
+                                                                                         resize_image_shape_list[image_index][0], resize_image_shape_list[image_index][1],
+                                                                                         input_image_shape_list[image_index][0],
+                                                                                         input_image_shape_list[image_index][1], output_count_tensor[image_index, pred_index])
+                resize_grad_cam_canvas_list.append(resize_grad_cam_canvas)
+
+
+            output_dict = {'score': [round(((1.0 - abs(round(count)-count))/2) / 0.5, 4) for count in output_count_tensor[image_index].tolist()],
+                           'count': [round(count) for count in output_count_tensor[image_index].tolist()],
+                           'cam': resize_cam_canvas,
+                           'grad_cam': resize_grad_cam_canvas_list}
             output_dict_list.append(output_dict)
         return output_dict_list
+
+
+    def __prepare_grad_model(self, layer_name_list=('Conv1', 'block_1_depthwise', 'block_3_depthwise', 'block_6_depthwise')):
+        output_layers = []
+        for layer_name in layer_name_list:
+            output_layers.append(self.model.get_layer(layer_name).output)
+        output_layers.append(self.model.output)
+        grad_model = tf.keras.models.Model([self.model.inputs], output_layers)
+        return grad_model
+
+    def __make_gradcam_heatmap(self, image_array, pred_index=None):
+        with tf.GradientTape(persistent=True) as tape:
+            outputs = self.grad_model(np.expand_dims(image_array, 0))
+            last_conv_layer_outputs = outputs[:-1]
+            preds = outputs[-1][0]
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
+        cam_list = []
+        for last_conv_layer_output in last_conv_layer_outputs:
+            grads = tape.gradient(class_channel, last_conv_layer_output)
+
+            pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+            last_conv_layer_output = last_conv_layer_output[0]
+            heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+            heatmap = tf.squeeze(heatmap)
+
+            heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+            cam_list.append(heatmap.numpy())
+        del tape
+        return cam_list
+
+    def __make_count_heatmap(self, image_array, crop_height, crop_width, resize_height, resize_width, total_num):
+        if np.all(image_array == 0.0):
+            return np.zeros((resize_height, resize_width), dtype=np.float32)
+        org_input_image = Image.fromarray(image_array).resize((self.model_input_shape[2], self.model_input_shape[1]), resample=Image.BICUBIC)
+        crop_image = org_input_image.crop((0, 0, crop_width, crop_height))
+        resize_crop_image = np.asarray(crop_image.resize((resize_width, resize_height)))
+        resize_crop_image = resize_crop_image - np.min(resize_crop_image)
+        resize_crop_image = np.clip(resize_crop_image, 0, np.max(resize_crop_image))
+        resize_crop_image = resize_crop_image * (total_num/np.sum(resize_crop_image))
+        return resize_crop_image
